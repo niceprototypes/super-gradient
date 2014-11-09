d25f7322536aced77b8ffa861060c6a4ee7c3d99
Mohammed Ibrahim <mi@thesilverlogic.com>
2014-11-09 01:07:38 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'demo/demo.css': 'demo/demo.scss'
+                }
+            }
+        },
+
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: 'demo/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['sass']);
+    grunt.registerTask('default', ['watch']);
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+<div style="text-align: center;">
+
+<h1 style="margin-bottom: 20px; font-size: 65px; font-weight: lighter;">SuperGradient</h1>
+
+<h3 style="padding-bottom: 20px; font-size: 13px; letter-spacing: 1px;"">SASS MULTI-COLOR GRADIENT MIXIN (LINEAR & RADIAL)</h3>
+
+<h4 style="margin: 70px 0 15px; font-size: 180%;">Options</h4>
+<p><strong style="color: #E5B025">$type: </strong>linear*, radial</p>
+
+<p><strong style="color: #E5B025">$from: </strong>top*, left, top left, top right, bottom right, bottom left, center (radial)</p>
+
+<p><strong style="color: #E5B025">$colors: </strong>hex, rgba</p>
+
+<h4 style="margin: 70px 0 15px; font-size: 180%;">Include</h4>
+<p><strong style="color: #E5B025">$type: </strong>linear*, radial</p>
+</div>
